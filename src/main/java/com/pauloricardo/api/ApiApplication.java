8d82baabedd1323cfcde502616a30d1f21414a77
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-17 08:46:06 -0300

    Configurando o projeto com as dependencias

--- src/main/java/com/pauloricardo/api/ApiApplication.java
+++ src/main/java/com/pauloricardo/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.pauloricardo.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
+        //Carregando o ENV
+        Dotenv dotenv = Dotenv.configure()
+                .filename(".env.test")
+                .load();
+        //Definindo Variaveis
+        System.setProperty("SPRING_PROFILE", dotenv.get("SPRING_PROFILE"));
+        System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
