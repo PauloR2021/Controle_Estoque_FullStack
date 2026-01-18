2ec288d6575ad3cdd71f4538aedb5cc5e2722012
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-18 10:08:36 -0300

    Adicionando o Services de Autenticacao

--- src/main/java/com/pauloricardo/api/Service/Autenticacao/AutenticacaoService.java
+++ src/main/java/com/pauloricardo/api/Service/Autenticacao/AutenticacaoService.java
@@ -1,4 +1,24 @@
 package com.pauloricardo.api.Service.Autenticacao;
 
-public class AutenticacaoService {
+import com.pauloricardo.api.Respository.UsuarioRepositrory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepositrory usuarioRepositrory;
+
+    //Veriifca que tipo de usuário é
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepositrory.findByUsername(username)
+                .orElseThrow( () -> new UsernameNotFoundException("Usuário Não Existe"));
+
+    }
 }
--- src/main/java/com/pauloricardo/api/Service/Autenticacao/TokenService.java
+++ src/main/java/com/pauloricardo/api/Service/Autenticacao/TokenService.java
@@ -1,4 +1,55 @@
 package com.pauloricardo.api.Service.Autenticacao;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.pauloricardo.api.Model.Usuario.UsuarioModel;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @PostConstruct
+    public void checkSecret(){
+        if(secret == null || secret.isBlank()){
+            throw new IllegalStateException("JWT secret Não Configurado");
+        }
+    }
+
+    public String gerarToken(UsuarioModel usuario){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(usuario.getUsername())
+                    .withExpiresAt(Instant.now().plusSeconds(7200))
+                    .sign(algorithm);
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token inválido ou expirado");
+        }
+    }
+
+
 }
