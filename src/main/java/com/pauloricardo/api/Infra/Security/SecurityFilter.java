6d3e324e98aaca1aaa67fc32036724f690b0fbee
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-18 10:06:39 -0300

    Adicionando os metodos de Security

--- src/main/java/com/pauloricardo/api/Infra/Security/SecurityFilter.java
+++ src/main/java/com/pauloricardo/api/Infra/Security/SecurityFilter.java
@@ -1,4 +1,53 @@
 package com.pauloricardo.api.Infra.Security;
 
-public class SecurityFilter {
+import com.pauloricardo.api.Respository.UsuarioRepositrory;
+import com.pauloricardo.api.Service.Autenticacao.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenServices;
+
+    @Autowired
+    private UsuarioRepositrory userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = recoverToken(request);
+        System.out.println("TOKEN: " +token);
+
+        if (token != null) {
+            String username = tokenServices.validateToken(token);
+            System.out.println("USERNAME DO TOKEN: " + username);
+
+            if (username != null) {
+                userRepository.findByUsername(username).ifPresent(user -> {
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    System.out.println("DEBUG: Authentication setado no SecurityContext");
+                });
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) return null;
+        return authHeader.substring(7).trim(); // remove "Bearer " e espaços extras
+    }
 }
