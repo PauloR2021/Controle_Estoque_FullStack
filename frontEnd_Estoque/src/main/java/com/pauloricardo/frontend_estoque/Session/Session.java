debbd16736ea7fdb2d7866108b5f16d205d32b9e
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-18 16:14:11 -0300

    Adicionando os Services responsavel pela autenticacao e validacao dos dados via API

--- frontEnd_Estoque/src/main/java/com/pauloricardo/frontend_estoque/Session/Session.java
+++ frontEnd_Estoque/src/main/java/com/pauloricardo/frontend_estoque/Session/Session.java
@@ -1,4 +1,24 @@
 package com.pauloricardo.frontend_estoque.Session;
 
 public class Session {
+    private static  String token;
+    private Session(){}
+
+    public static void setToken(String jwt){
+        token = jwt;
+    }
+
+    public static String getToken(){
+        return token;
+    }
+
+    public static boolean isAuthenticated() {
+        return token != null && !token.isBlank();
+    }
+
+    public static void clear() {
+        token = null;
+    }
+
+
 }
