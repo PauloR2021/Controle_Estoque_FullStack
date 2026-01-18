2380f0eefd541d837921c6a6c93410e8bb44659b
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-18 10:09:26 -0300

    Adicionando o Respostitory do Usuario

--- src/main/java/com/pauloricardo/api/Respository/UsuarioRepositrory.java
+++ src/main/java/com/pauloricardo/api/Respository/UsuarioRepositrory.java
@@ -3,5 +3,9 @@ package com.pauloricardo.api.Respository;
 import com.pauloricardo.api.Model.Usuario.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepositrory extends JpaRepository<UsuarioModel, Integer> {
+    //Veririca se o Usuário Existe
+    Optional<UsuarioModel> findByUsername(String username);
 }
