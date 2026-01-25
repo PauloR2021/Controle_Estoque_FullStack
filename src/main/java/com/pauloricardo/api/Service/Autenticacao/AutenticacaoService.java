aa9827c030a14c3b2c1fb69da51292f527547724
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-25 20:57:09 -0300

    Adicionando a autenticacao do backend

--- src/main/java/com/pauloricardo/api/Service/Autenticacao/AutenticacaoService.java
+++ src/main/java/com/pauloricardo/api/Service/Autenticacao/AutenticacaoService.java
@@ -1,7 +1,9 @@
 package com.pauloricardo.api.Service.Autenticacao;
 
+import com.pauloricardo.api.Model.Usuario.UsuarioModel;
 import com.pauloricardo.api.Respository.UsuarioRepositrory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,8 +19,15 @@ public class AutenticacaoService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepositrory.findByUsername(username)
-                .orElseThrow( () -> new UsernameNotFoundException("Usuário Não Existe"));
+
+        UsuarioModel usuario = usuarioRepositrory.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        if (!usuario.isAtivo()) {
+            throw new DisabledException("Usuário não está ativo");
+        }
+
+        return usuario;
 
     }
 }
