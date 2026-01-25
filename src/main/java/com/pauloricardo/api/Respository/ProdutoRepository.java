7666702cbecedb4eba744788b8043d4892bad9c1
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-25 20:56:39 -0300

    Adicionando o Repository

--- src/main/java/com/pauloricardo/api/Respository/ProdutoRepository.java
+++ src/main/java/com/pauloricardo/api/Respository/ProdutoRepository.java
@@ -4,4 +4,8 @@ import com.pauloricardo.api.Model.Produto.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Integer> {
+
+    boolean existsByCodigoBarras(String codigoBarras);
+
+    boolean existsByCodigoBarrasAndIdNot(String s, Integer id);
 }
--- src/main/java/com/pauloricardo/api/Respository/UsuarioRepositrory.java
+++ src/main/java/com/pauloricardo/api/Respository/UsuarioRepositrory.java
@@ -8,4 +8,9 @@ import java.util.Optional;
 public interface UsuarioRepositrory extends JpaRepository<UsuarioModel, Integer> {
     //Veririca se o Usuário Existe
     Optional<UsuarioModel> findByUsername(String username);
+
+    Optional<UsuarioModel> findByEmail(String email);
+
+    // Verifica se o Username existe
+    boolean existsByUsername(String username);
 }
