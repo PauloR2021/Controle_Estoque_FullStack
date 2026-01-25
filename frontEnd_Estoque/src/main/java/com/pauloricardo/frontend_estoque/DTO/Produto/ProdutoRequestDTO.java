cd64b0c09eeab482e721a04a8aa205eeecdc5c0d
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-25 20:49:40 -0300

    Criando os DTOs para os Produtos

--- frontEnd_Estoque/src/main/java/com/pauloricardo/frontend_estoque/DTO/Produto/ProdutoRequestDTO.java
+++ frontEnd_Estoque/src/main/java/com/pauloricardo/frontend_estoque/DTO/Produto/ProdutoRequestDTO.java
@@ -1,4 +1,61 @@
 package com.pauloricardo.frontend_estoque.DTO.Produto;
 
+import java.math.BigDecimal;
+
 public class ProdutoRequestDTO {
+
+    String nome;
+    String codigoBarras;
+    Integer quantidade;
+    Integer quantidadeMininma;
+    BigDecimal preco;
+    Boolean ativo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Integer getQuantidadeMininma() {
+        return quantidadeMininma;
+    }
+
+    public void setQuantidadeMininma(Integer quantidadeMininma) {
+        this.quantidadeMininma = quantidadeMininma;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
 }
--- frontEnd_Estoque/src/main/java/com/pauloricardo/frontend_estoque/DTO/Produto/ProdutoResponseDTO.java
+++ frontEnd_Estoque/src/main/java/com/pauloricardo/frontend_estoque/DTO/Produto/ProdutoResponseDTO.java
@@ -1,4 +1,51 @@
 package com.pauloricardo.frontend_estoque.DTO.Produto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+
 public class ProdutoResponseDTO {
+    private  Integer id;
+    private  String nome;
+    private  String codigoBarras;
+    private  BigDecimal preco;
+    private  Integer quantidade;
+    private  Integer quantidadeMinima;
+    private  Boolean ativo;
+    private  LocalDateTime criacao;
+
+    public ProdutoResponseDTO() {
+    }
+
+    public ProdutoResponseDTO(
+            Integer id,
+            String nome,
+            String codigoBarras,
+            BigDecimal preco,
+            Integer quantidade,
+            Integer quantidadeMinima,
+            Boolean ativo,
+            LocalDateTime criacao
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.codigoBarras = codigoBarras;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.quantidadeMinima = quantidadeMinima;
+        this.ativo = ativo;
+        this.criacao = criacao;
+    }
+
+    public Integer getId() { return id; }
+    public String getNome() { return nome; }
+    public String getCodigoBarras() { return codigoBarras; }
+    public BigDecimal getPreco() { return preco; }
+    public Integer getQuantidade() { return quantidade; }
+    public Integer getQuantidadeMinima() { return quantidadeMinima; }
+    public Boolean getAtivo() { return ativo; }
+    public LocalDateTime getCriacao() { return criacao; }
 }
