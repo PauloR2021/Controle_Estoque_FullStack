d7a83ae8d28ebf1660341b298dee783af99acdd5
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-01-25 21:04:02 -0300

    Adicioando o module do Front

--- frontEnd_Estoque/src/main/java/module-info.java
+++ frontEnd_Estoque/src/main/java/module-info.java
@@ -1,14 +1,24 @@
 module com.pauloricardo.frontend_estoque {
+
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
+    requires javafx.base;
+
     requires java.net.http;
+    requires java.desktop;
+
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires javafx.graphics;
 
-
-    // Abre o pacote de controllers para o FXMLLoader
-    opens com.pauloricardo.frontend_estoque.Controller to javafx.fxml;
-
-    // Exporta o pacote principal (MainApp)
     exports com.pauloricardo.frontend_estoque;
+
+    // JavaFX
+    opens com.pauloricardo.frontend_estoque.Controller to javafx.fxml;
+    opens com.pauloricardo.frontend_estoque.Controller.Produto to javafx.fxml;
+
+    opens com.pauloricardo.frontend_estoque.DTO.Produto
+            to javafx.base, com.fasterxml.jackson.databind;
+
+    opens com.pauloricardo.frontend_estoque.styles;
 }
